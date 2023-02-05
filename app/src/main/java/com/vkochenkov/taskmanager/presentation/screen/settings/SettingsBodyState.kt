36e132c9c86cb04f30d3cfb096f94d37e0482eac
vkochenkov <kochenkov1993@gmail.com>
2023-02-05 16:49:18 +0300

    refactor SettingsBodyState

--- app/src/main/java/com/vkochenkov/taskmanager/presentation/screen/settings/SettingsBodyState.kt
+++ app/src/main/java/com/vkochenkov/taskmanager/presentation/screen/settings/SettingsBodyState.kt
@@ -1,21 +1,18 @@
 package com.vkochenkov.taskmanager.presentation.screen.settings
 
+data class SettingsBodyState(
+    val statuses: List<String>,
+    val showNewStatusDialog: Boolean = false,
+    val showCantDeleteStatusDialog: ReasonCantDeleteStatus? = null,
+    val loadingStatusIndex: Int? = null,
+    val renameStatusIndex: Int? = null
+) {
 
-sealed class SettingsBodyState {
-
-    data class Content(
-        val statuses: List<String>,
-        val showNewStatusDialog: Boolean = false,
-        val showCantDeleteStatusDialog: ReasonCantDeleteStatus? = null,
-        val loadingStatusIndex: Int? = null,
-        val renameStatusIndex: Int? = null
-    ) : SettingsBodyState() {
-
-        enum class ReasonCantDeleteStatus {
-            LAST,
-            SAME
-        }
+    enum class ReasonCantDeleteStatus {
+        LAST,
+        SAME
     }
 }
+
 
 
