4ca40a12596f9b88fb665254b28206e889a86684
vkochenkov <kochenkov1993@gmail.com>
2023-02-05 19:01:25 +0300

    preparation for drug and drop

--- app/src/main/java/com/vkochenkov/taskmanager/presentation/screen/settings/SettingsBody.kt
+++ app/src/main/java/com/vkochenkov/taskmanager/presentation/screen/settings/SettingsBody.kt
@@ -1,7 +1,10 @@
 package com.vkochenkov.taskmanager.presentation.screen.settings
 
 import android.content.res.Configuration
+import android.view.MotionEvent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,20 +15,34 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import com.vkochenkov.taskmanager.R
 import com.vkochenkov.taskmanager.presentation.theme.TaskManagerTheme
+import kotlin.math.roundToInt
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class,
+    ExperimentalComposeUiApi::class
+)
 @Composable
 fun SettingsBody(
     state: SettingsBodyState,
     onAction: (SettingsActions) -> Unit
 ) {
+
+    var stats by remember {
+        mutableStateOf(state.statuses)
+    }
+
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
@@ -234,7 +251,12 @@ fun SettingsBody(
                                     isErrorSymbols = false
                                     if (!state.statuses.contains(statusStr)) {
                                         isErrorSameItem = false
-                                        onAction.invoke(SettingsActions.RenameStatus(statusStr, index))
+                                        onAction.invoke(
+                                            SettingsActions.RenameStatus(
+                                                statusStr,
+                                                index
+                                            )
+                                        )
                                     } else {
                                         isErrorSameItem = true
                                     }
@@ -257,7 +279,6 @@ fun SettingsBody(
                     item {
                         Divider()
                     }
-                    // todo add drag and drop for change status position
                     item {
                         Row(
                             modifier = Modifier.fillMaxWidth(),
@@ -271,8 +292,85 @@ fun SettingsBody(
                                 Icon(Icons.Default.Add, contentDescription = null)
                             }
                         }
-                        state.statuses.forEachIndexed { index, status ->
+
+                        stats.forEachIndexed { index, status ->
+
+                            var offsetY by remember { mutableStateOf(0f) }
+
+                            var wasDragged by remember { mutableStateOf(false) }
+
+                            // Create element height in pixel state
+                            var columnHeightPx by remember {
+                                mutableStateOf(0f)
+                            }
+
+                            // todo improve grug and drop solution
+
                             AssistChip(
+                                modifier = Modifier
+                                    .onGloballyPositioned { coordinates ->
+                                        columnHeightPx = coordinates.size.height.toFloat()
+                                    }
+                                    .offset { IntOffset(0, offsetY.roundToInt()) }
+//                                    .draggable(
+//                                        orientation = Orientation.Vertical,
+//                                        state = rememberDraggableState { delta ->
+//                                            if (!wasDragged) {
+//                                                offsetY += delta
+//                                                Log.d("vladd", offsetY.toString())
+//                                                if (offsetY > columnHeightPx / 2) {
+//                                                    val newStats = stats.toMutableList()
+//                                                    val cur = newStats.get(index)
+//                                                    newStats.removeAt(index)
+//                                                    newStats.add(index + 1, cur)
+//                                                    stats = newStats
+//                                                    offsetY = 0f
+//                                                    wasDragged = true
+//                                                } else if (offsetY < -columnHeightPx / 2) {
+//                                                    val newStats = stats.toMutableList()
+//                                                    val cur = newStats.get(index)
+//                                                    newStats.removeAt(index)
+//                                                    newStats.add(index - 1, cur)
+//                                                    stats = newStats
+//                                                    offsetY = 0f
+//                                                    wasDragged = true
+//                                                }
+//                                            }
+//                                        }
+//                                    )
+
+                                    .pointerInput(Unit) {
+                                        detectDragGestures { change, dragAmount ->
+                                            if (!wasDragged) {
+                                                offsetY += dragAmount.y
+                                                if (offsetY > columnHeightPx / 1.5) {
+                                                    val newStats = stats.toMutableList()
+                                                    val cur = newStats.get(index)
+                                                    newStats.removeAt(index)
+                                                    newStats.add(index + 1, cur)
+                                                    stats = newStats
+                                                    offsetY = 0f
+                                                    wasDragged = true
+                                                } else if (offsetY < -columnHeightPx / 1.5) {
+                                                    val newStats = stats.toMutableList()
+                                                    val cur = newStats.get(index)
+                                                    newStats.removeAt(index)
+                                                    newStats.add(index - 1, cur)
+                                                    stats = newStats
+                                                    offsetY = 0f
+                                                    wasDragged = true
+                                                }
+                                            }
+                                        }
+                                    }
+                                    // todo fix clickable
+                                    .pointerInteropFilter {
+                                        wasDragged = when (it.action) {
+                                            MotionEvent.ACTION_UP -> false
+                                            else -> false
+                                        }
+                                        true
+                                    },
                                 onClick = {
                                     onAction.invoke(SettingsActions.ShowRenameStatusDialog(index))
                                 },
