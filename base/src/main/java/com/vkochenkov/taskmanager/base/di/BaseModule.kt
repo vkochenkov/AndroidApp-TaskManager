bd15d590fb6ea6fafdc055096864b3d8d9d742c2
vkochenkov <kochenkov1993@gmail.com>
2023-03-06 20:44:34 +0300

    remove unnecessary package names

--- base/src/main/java/com/vkochenkov/taskmanager/base/di/BaseModule.kt
+++ base/src/main/java/com/vkochenkov/taskmanager/base/di/BaseModule.kt
@@ -2,7 +2,10 @@ package com.vkochenkov.taskmanager.base.di
 
 import android.content.Context
 import androidx.room.Room
+import com.vkochenkov.taskmanager.base.data.StatusPreferences
 import com.vkochenkov.taskmanager.base.data.StatusPreferences.Companion.STATUSES_PREFS_KEY
+import com.vkochenkov.taskmanager.base.data.TaskDataService
+import com.vkochenkov.taskmanager.base.data.db.AppDatabase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -12,19 +15,19 @@ val baseModule = module {
     single {
         Room.databaseBuilder(
             androidContext(),
-            com.vkochenkov.taskmanager.base.data.db.AppDatabase::class.java, "task_manager_db"
+            AppDatabase::class.java, "task_manager_db"
         ).fallbackToDestructiveMigration()
             .build()
     }
 
     // TaskDao
     single {
-        val database = get<com.vkochenkov.taskmanager.base.data.db.AppDatabase>()
+        val database = get<AppDatabase>()
         database.taskDao()
     }
 
     // TaskRepository
-    single { com.vkochenkov.taskmanager.base.data.TaskDataService(get()) }
+    single { TaskDataService(get()) }
 
     // Status prefs
     single {
@@ -33,6 +36,6 @@ val baseModule = module {
 
     // StatusRepository
     single {
-        com.vkochenkov.taskmanager.base.data.StatusPreferences(get())
+        StatusPreferences(get())
     }
 }
